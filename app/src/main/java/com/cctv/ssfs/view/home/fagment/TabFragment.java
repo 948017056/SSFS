4a34b4f6551d3bcc46e1c3732e8fc6b0fd17d4a4
qi <948017056@qq.com>
2018-05-09 11:28:34 +0800

    第一版提交

--- app/src/main/java/com/cctv/ssfs/view/home/fagment/TabFragment.java
+++ app/src/main/java/com/cctv/ssfs/view/home/fagment/TabFragment.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.LinearLayout;
 
 import com.cctv.ssfs.R;
 import com.cctv.ssfs.adapter.HomeRecyclerAdapter;
